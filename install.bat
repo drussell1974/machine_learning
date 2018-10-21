677d19c121e5e7c71f0e47d46513cb6654e12fa1
Mr Russell <drussell1974@users.noreply.github.com>
2018-10-21 05:25:29 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,14 +4,14 @@ export ML_PATH="$HOME/dev/machine_learning"
 mkdir -p $ML_PATH
 
 echo "upgrading pip..."
-pip3 install --upgrade pip
+sudo ip3 install --upgrade pip
 
 echo "Installing virtual environment"
-pip3 install --user -upgrade virtualenv
+sudo pip3 install --user -upgrade virtualenv
 
 echo "Creating virtual environment..."cd $ML_PATH
 virtualenv env
 source env/bin/activate
 
 echo "Installing jupyter and other libraries"
-pip3 install --upgrade jupyter matplotlib numpy pandas scipy scikit-learn
+sudo pip3 install --upgrade jupyter matplotlib numpy pandas scipy scikit-learn
